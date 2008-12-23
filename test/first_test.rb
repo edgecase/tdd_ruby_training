e53c879047af4e5aedf8fe77ae8a97f9dc203a42
Joe O'Brien <joe@theedgecase.com>
2008-12-23 15:39:23 -0500

    adding more tests to array and cleaninig up the test names

--- test/first_test.rb
+++ test/first_test.rb
@@ -1,9 +1,5 @@
 require 'test/unit'
 
-def __
-  "FILL ME IN"
-end
-
 class TestSomething < Test::Unit::TestCase
   def test_assert
     assert true
@@ -12,17 +8,4 @@ class TestSomething < Test::Unit::TestCase
   end
 end
 
-CodeMash = Test::Unit
 
-class ArrayTest < CodeMash::TestCase
-  def test_food
-    food = [:peanut, :button, :and, :jelly]
-    assert_equal __, food[0]
-    assert_equal __, food.size
-  end
-
-  def test_food_with_numbers
-    food = [:peanut, :button, :and, :jelly, 1, nil]
-    assert_equal __, food.size
-  end
-end
