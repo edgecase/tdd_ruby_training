69ba8fa64d59ceff2abd1185b8b0c1e89d99b19e
Joe O'Brien <joe@theedgecase.com>
2009-01-23 21:41:21 -0500

    working on the readme file and getting it ready for general use

--- koans/about_nil.rb
+++ koans/about_nil.rb
@@ -6,9 +6,26 @@ class AboutNil < CodeMash::Koan
   end
 
   def test_you_dont_get_null_pointer_errors_when_calling_methods_on_nil
+    #
+    #  What is the Exception that is thrown when you call a method that
+    #  does not exist?  
+    #
+    #  Hint:  launch irb and try the code in the block below.  
+    # 
+    #  Don't be confused by the code below yet.  It's using blocks 
+    #  which are explained later on in about_blocks.rb.  For now,
+    #  think about it like running nil.some_method_nil_doesnt_know_about
+    #  in a sandbox and catching the error class into the exception
+    #  variable.  
+    #
     exception = assert_raise(___) do
       nil.some_method_nil_doesnt_know_about
     end
+    
+    # 
+    #  What is the error message itself? What substring or pattern could 
+    #  you test against in order to have a good idea what the string is?
+    #  
     assert_match /__/, exception.message
   end
 
